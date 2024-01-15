f87ff7efdf547c895557ea58768fdd2704c1b5e3
Humphrey Singculan <Humfurie@gmail.com>
2024-01-15 19:43:31 +0800

    adding user seeder

---
+++ backend/domain/Users/database/seeders/UserSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Domain\Users\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+    }
+}
