77e82d3752a770b1d97efab01df8097b71c6f45b
Humphrey Singculan <humfurie@gmail.com>
2024-02-01 20:15:12 +0800

    refactor
    Signed-off-by: Humphrey Singculan <humfurie@gmail.com>

--- backend/domain/Permissions/database/factories/PermissionFactory.php
+++ backend/domain/Permissions/database/factories/PermissionFactory.php
@@ -2,13 +2,16 @@
 
 namespace Domain\Permissions\database\factories;
 
+use Domain\Permissions\Models\Permission;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Domain\Permissions\Models\Permission>
  */
 class PermissionFactory extends Factory
 {
+    protected $model = Permission::class;
+
     /**
      * Define the model's default state.
      *
--- backend/domain/Roles/database/factories/RoleFactory.php
+++ backend/domain/Roles/database/factories/RoleFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\Domain\Roles\Models\Role>
  */
 class RoleFactory extends Factory
 {
